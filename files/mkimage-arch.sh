30c8cc151a4a30e78692c019662d09688705e272
ChihChieh Huang <ChihChieh_Huang@htc.com>
2016-02-16 19:29:24 +0800

    Use tar command from archlinux instead of busybox

--- files/mkimage-arch.sh
+++ files/mkimage-arch.sh
@@ -88,4 +88,4 @@ mknod -m 600 $DEV/initctl p
 mknod -m 666 $DEV/ptmx c 5 2
 ln -sf /proc/self/fd $DEV/fd
 
-tar --numeric-owner --xattrs --acls -C $ROOTFS -c . > root.tar
+/usr/bin/tar --numeric-owner --xattrs --acls -C $ROOTFS -c . > root.tar
